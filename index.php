cb8ba3ceea2221b9b96e327c90a47bbf014f1dfe
MnqobiSB <amandlamm1@gmail.com>
2021-10-20 13:08:17 +0200

    display mysql data on an html table using php

--- index.php
+++ index.php
@@ -13,7 +13,26 @@
   $result = mysqli_query($connect,"SELECT * FROM employees");
 
   //Loop Through Result & echo data
-  while($row = mysqli_fetch_array($result)) {
+  /* while($row = mysqli_fetch_array($result)) {
     echo $row['first_name'] . " " . $row['last_name']."<br />";
-  } 
+  } */
 ?>
+
+<table width="500" cellpadding="5" cellspacing="5" border="1">
+  <tr>
+    <th>ID#</th>
+    <th>First Name</th>
+    <th>Last Name</th>
+    <th>Department</th>
+    <th>Email</th>
+  </tr>
+  <?php while($row = mysqli_fetch_array($result)) : ?>
+  <tr>
+    <td><?php echo $row['id'] ?></td>
+    <td><?php echo $row['first_name'] ?></td>
+    <td><?php echo $row['last_name'] ?></td>
+    <td><?php echo $row['department'] ?></td>
+    <td><?php echo $row['email'] ?></td>
+  </tr>
+  <?php endwhile; ?>
+</table>
